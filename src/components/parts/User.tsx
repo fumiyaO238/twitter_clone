2019c3a8a0d545df3d8d0e313acab033d4cf5329
fumiyaO238 <f_otsuka@two-six.jp>
2024-06-10 14:53:59 +0900

    各ユーザへ遷移可能

--- src/components/parts/User.tsx
+++ src/components/parts/User.tsx
@@ -1,16 +1,94 @@
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { UserType } from "./UserList";
+import { BlogType } from "./BlogList";
+import Header from "../view/Header";
+import axios from "axios";
+import Footer from "../view/Footer";
+
 
 const User = () => {
+  const [myBlogs, setMyBlogs] = useState<BlogType[]>([]);
+  const [myProfile, setMyProfile] = useState<UserType[]>([]);
+  const [userName, setUserName] = useState<string>("");
+
   const location = useLocation();
+
   const pn = location.pathname;
   const prm = pn.split("/");
-  const userId = prm[2]
+  const checkUserId = prm[2]  
 
+  // ブログの削除
+  const deleteBlog = async (id: number) => {
+    // console.log(id)
+
+    await axios
+      .delete("http://localhost:3333/delete", {
+        data: { id }
+      })
+      .then((response) => {
+        console.log(response);
+        const newBlogs = myBlogs.filter((blog) => blog.id !== id);
+        setMyBlogs(newBlogs);
+      })
+      .catch((e) => {
+        console.log(e.message);
+        setMyBlogs(myBlogs);
+      })
+  }
+
+  // getリクエスト
+  useEffect(() => {
+    axios.get("http://localhost:3333/another_blogs",
+      {
+        headers: {
+          Authorization : `Bearer ${checkUserId}`
+        }
+      }
+    )
+      .then((response) => {
+        const myBlogs = response.data.blogResult;
+        const myProfile = response.data.userResult;
+        setMyBlogs(myBlogs);
+        setMyProfile(myProfile)
+        setUserName(myProfile[0].name);
+      });
+  }, []);
   return (
     <div>
-      <h3>ユーザーID:{userId}</h3>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="bloglist">
+        {/* タイトル */}
+        <nav className="navbar">
+          <h1>MyBlogList</h1>
+          <div className="links">
+            <a href="/create">New Blog</a>
+          </div>
+        </nav>
+        <div className="blog-list">
+          <div className="blogs">
+            {myBlogs.map((blog) => (
+              <div className="blog-preview" key={blog.id} >
+                <p className="author">投稿者 : {userName}</p>
+                <h4>{blog.content}</h4>
+                <span style={{ display: "flex" }}>
+                  <p className="date">投稿日時 : {blog.created_at}</p>
+                  <button className="button" onClick={() => deleteBlog(blog.id)}>
+                    delete
+                  </button>
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="header">
+        <Footer />
+      </div>
     </div>
   )
 }
 
-export default User
+export default User;
